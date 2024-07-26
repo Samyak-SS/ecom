b7deacc82b3f4636a8595df3a8384b1745329a8a
Samyak <mail4samyak@gmail.com>
2024-07-27 03:14:41 +0530

    home page and search and filter done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navigation from "./navigation/Nav";
+import Products from "./Products/Products"
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+
+
+
+//databse
+import products from './db/data';
+import Category from "./Sidebar/Category/Category";
+import Card from "./components/Card";
 
 function App() {
+
+  const[selectedCategory,setSelectedCategory]=useState(null);
+  const[query,setQuery] = useState("")
+  //------------input filter-----------
+  
+
+  const handleInputChange = event =>{
+    setQuery(event.target.value)
+    
+  }
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+); 
+
+   //------------Radio filter-----------
+   const handleChange = event =>{
+      setSelectedCategory(event.target.value) 
+   } 
+  
+   //------------Button filter-----------
+   const handleClick = event =>{
+    setSelectedCategory(event.target.value)
+   }
+   //------------FAUNCTIONMNMN-----------
+   function filteredData(products,selected,query){
+    let filteredProducts = products;
+
+    //filtering input items
+    if(query){
+      filteredProducts = filteredItems;
+      
+    }
+
+    //Selected Filter
+    if(selected){
+      filteredProducts = filteredProducts.filter(({category,color,company,
+         newPrice,title}) => 
+        category === selected ||
+        color === selected ||
+        company === selected ||
+        newPrice === selected ||
+        title === selected
+      );
+    }
+      return filteredProducts.map(({img,title,star,reviews,prevPrice,newPrice,})=>(
+        <Card 
+        key={Math.random()}
+        img = {img}
+        title = {title}
+        star = {star}
+        reviews = {reviews}
+        prevPrice = {prevPrice}
+        newPrice = {newPrice}
+        
+        
+        />
+      ))
+
+   }
+
+   const result = filteredData(products,selectedCategory,query)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Sidebar handleChange={handleChange}/>
+    <Navigation query={query} handleInputChange={handleInputChange}/>
+    
+    <Recommended handleClick={handleClick}/>
+    <Products result={result}/>
+    </>
   );
 }
 
---
+++ src/Products/Products.js
@@ -0,0 +1,19 @@
+import { AiFillStar   } from 'react-icons/ai';
+import { IoBagSharp } from 'react-icons/io5';
+import './Products.css'
+
+ function Products({result}) {
+  return (
+    <>
+    <section className='card-container'>
+      {result}
+      
+    </section>
+    </>
+  )
+}
+
+
+export default Products;
+
+
